91b0b0abdfe7b87c669dd67f3eaeef2caf2dc598
JasonRadcliffe <jasonjradcliffe@gmail.com>
2014-04-22 12:35:44 -0600

    Attempt #1 at manipulating form data
    the getText() method is currently crashing the program :(

--- src/com/furnacechecks/MainActivity.java
+++ src/com/furnacechecks/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity{
 	private Button mNewFormButton;
 	private Button mViewFormsButton;
 	
+	
 	@Override
 	protected void onCreate (Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
--- src/com/furnacechecks/NewForm.java
+++ src/com/furnacechecks/NewForm.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class NewForm extends Activity {
 	
 	private Button msaveButton;
+	private TextView mIndoorMakeAnswerText;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -24,6 +26,14 @@ public class NewForm extends Activity {
 				 *   2)save to XML doc
 				 *   3) display some screen?
 				 */
+				
+				//extract data test - This currently crashes app
+				String dataExtracted;
+				mIndoorMakeAnswerText = (TextView) findViewById(R.id.IndoorMake);
+				/*THIS LINE CRASHES IT*/dataExtracted = (String) mIndoorMakeAnswerText.getText();
+				dataExtracted+="Jason";
+				System.out.println(dataExtracted);
+				mIndoorMakeAnswerText.setText(dataExtracted);
 				
 			}
 		});
