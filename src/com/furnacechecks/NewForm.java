db1b58539c51363e1cb2798f818431741f6171de
Squishmonsta <tuba.ritchie@yahoo.com>
2014-04-28 11:04:16 -0400

    input and output starts
    not much, just some added input and output streams. Locations are not
    set yet. Need to extract ID or Filename to identify file name.

--- src/com/furnacechecks/NewForm.java
+++ src/com/furnacechecks/NewForm.java
@@ -1,7 +1,14 @@
 package com.furnacechecks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,11 +29,6 @@ public class NewForm extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				/*save actions:
-				 * 	 1)extract data
-				 *   2)save to XML doc
-				 *   3) display some screen?
-				 */
 				
 				//Example of getting and setting form data
 				String formTitleString = "";
@@ -35,10 +37,34 @@ public class NewForm extends Activity {
 				formTitleString+="Jason";
 				mFormTitle.setText(formTitleString);
 				
-				
-				
-				
-				
+				/*save actions:
+				 * 	 1)extract data
+				 *   2)save to XML doc *Baseline code*
+				 *   3) display some screen?
+				 */
+				//2
+				String xmlFile = "<form> <title>" + mFormTitle + "</title> </form>";
+				try {
+					FileOutputStream fos = openFileOutput(mFormTitle + ".xml",
+					        Context.MODE_APPEND | Context.MODE_WORLD_READABLE);
+					fos.write(xmlFile.getBytes());
+					fos.close();
+					
+					String storageState = Environment.getExternalStorageState();
+				    if (storageState.equals(Environment.MEDIA_MOUNTED)) {
+				        File file = new File(getExternalFilesDir(null),
+				                "DayTwentyTwoFileTwo");
+				        FileOutputStream fos2 = new FileOutputStream(file);
+				        fos2.write(xmlFile.getBytes());
+				        fos2.close();
+				    }
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 		
--- src/com/furnacechecks/ViewForms.java
+++ src/com/furnacechecks/ViewForms.java
@@ -1,7 +1,14 @@
 package com.furnacechecks;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 
 public class ViewForms extends Activity {
 	
@@ -12,11 +19,19 @@ public class ViewForms extends Activity {
 		
 		//TODO: Open resource folder where forms are kept and display list of available forms
 		
+		String formId = "blah"; //Need to access the onclick variable to extract the correct form
 		
+		try {
+		    BufferedReader inputReader = new BufferedReader(new InputStreamReader(
+		            openFileInput(formId + ".xml")));
+		    String inputString;
+		    StringBuffer stringBuffer = new StringBuffer();                
+		    while ((inputString = inputReader.readLine()) != null) {
+		        stringBuffer.append(inputString + "\n");
+		    }
+			lblTextViewOne.setText(stringBuffer.toString());
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		}
 	}
-	
-	
-	
-	
-
-}
