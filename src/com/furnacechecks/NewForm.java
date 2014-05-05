9f634f22ee47927edd2ec6f807dfdd75dba0a527
JasonRadcliffe <jasonjradcliffe@gmail.com>
2014-05-04 19:27:36 -0600

    Finished data extraction
    finished the xml extraction for new forms
    cleaned up some
    new form dumps you on main activity upon save

--- src/com/furnacechecks/NewForm.java
+++ src/com/furnacechecks/NewForm.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,6 @@ import android.widget.EditText;
 public class NewForm extends Activity {
 	
 	private Button msaveButton;
-	private EditText mIndoorMakeAnswerText;
 	private EditText mFormTitle;
 	
 	@Override
@@ -26,6 +26,12 @@ public class NewForm extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				
+				/*save actions:
+				 * 	 1)extract data
+				 *   2)save to XML doc
+				 *   3) display some screen?
+				 */
 				
 				
 				String formTitleString = "";
@@ -60,23 +66,90 @@ public class NewForm extends Activity {
 				String Width = getResources().getString(R.string.Width);
 				String Height = getResources().getString(R.string.Height);
 				
+				EditText EindoorMake = (EditText)findViewById(R.id.IndoorMake);
+				String indoorMake = EindoorMake.getText().toString();
+				
+				EditText EindoorModel = (EditText)findViewById(R.id.IndoorModel);
+				String indoorModel = EindoorModel.getText().toString();
+				
+				EditText EindoorSerial = (EditText)findViewById(R.id.IndoorSerial);
+				String indoorSerial = EindoorSerial.getText().toString();
+				
+				EditText EoutdoorMake = (EditText)findViewById(R.id.OutdoorMake);
+				String outdoorMake = EoutdoorMake.getText().toString();
+				
+				EditText EoutdoorModel = (EditText)findViewById(R.id.OutdoorModel);
+				String outdoorModel = EoutdoorModel.getText().toString();
+				
+				EditText EoutdoorSerial = (EditText)findViewById(R.id.OutdoorSerial);
+				String outdoorSerial = EoutdoorSerial.getText().toString();
+				
+				EditText EsupplyAir = (EditText)findViewById(R.id.SupplyAir);
+				String supplyAir = EsupplyAir.getText().toString();
+				
+				EditText EfluGasses = (EditText)findViewById(R.id.FluGasses);
+				String fluGasses = EfluGasses.getText().toString();
+				
+				EditText EindoorTempRise = (EditText)findViewById(R.id.IndoorTempRise);
+				String indoorTempRise = EindoorTempRise.getText().toString();
+				
+				EditText EoutdoorTempRise = (EditText)findViewById(R.id.OutdoorTempRise);
+				String outdoorTempRise = EoutdoorTempRise.getText().toString();
+				
+				EditText EindoorHigh = (EditText)findViewById(R.id.IndoorHigh);
+				String indoorHigh = EindoorHigh.getText().toString();
+				
+				EditText EindoorLow = (EditText)findViewById(R.id.IndoorLow);
+				String indoorLow = EindoorLow.getText().toString();
+				
+				EditText EoutdoorHigh = (EditText)findViewById(R.id.OutdoorHigh);
+				String outdoorHigh = EoutdoorHigh.getText().toString();
+				
+				EditText EoutdoorLow = (EditText)findViewById(R.id.OutdoorLow);
+				String outdoorLow = EoutdoorLow.getText().toString();
+				
+				EditText EgasPressure = (EditText)findViewById(R.id.GasPressure);
+				String gasPressure = EgasPressure.getText().toString();
+				
+				EditText EfilterLength = (EditText)findViewById(R.id.FilterLength);
+				String filterLength = EfilterLength.getText().toString();
+				
+				EditText EfilterWidth = (EditText)findViewById(R.id.FilterWidth);
+				String filterWidth = EfilterWidth.getText().toString();
+				
+				EditText EfilterHeight = (EditText)findViewById(R.id.FilterHeight);
+				String filterHeight = EfilterHeight.getText().toString();
+				
+				
+				
+				
+				
+				
 				
 				/*save actions:
-				 * 	 1)extract data //DONE
-				 *   2)save to XML doc *Baseline code added*
+				 * 	 1)extract data
+				 *   2)save to XML doc
 				 *   3) display some screen?
 				 */
 				//2
 				
 				
-				String xmlFile = "<form> <title>"+mFormTitle+"</title> <file> <IU>"+IndoorUnit+"</IU> <Make>"+Make+"</Make> <Model"+Model+"</Model> " +
-						"<SerialNum>"+SerialNumber+"</SerialNum> <OU>"+OutdoorUnit+"</OU> <CORead>"+COReading+"</CORead> <Air>"+SupplyAir+
-						"</Air> <FluGas>"+FluGasses+"</FluGas> <TempRise>"+TemperatureRise+"</TempRise> <InRise>"+IndoorRise+"</InRise> <OutRise>"+
-						OutdoorRise+"</OutRise> <Operating>"+OperatingRange+"</Operating> <High>"+High+"</High> <Low>"+Low+"</Low> " +
-						"<GasPresure>"+GasPresure+"</GasPresure> <GP>"+GP+"</GP> <FilterSize>"+FilterSize+"</FilterSize> <Length>"+Length
-						+"</Length> <Width>"+Width+"</Width> <Height>"+Height+"</Heigth> </file> </form>";
+				String xmlFile = "<form> <title>"+formTitleString+"</title> \n"
+						+ "<indoorMake>"+indoorMake+"</indoorMake> <indoorModel>"+indoorModel+"</indoorModel> " +
+						"<indoorSerial>"+indoorSerial+"</indoorSerial> \n"
+						+ "<outdoorMake>"+outdoorMake+"</outdoorMake> <outdoorModel>"+outdoorModel+"</outdoorModel> " +
+						"<outdoorSerial>"+outdoorSerial+"</outdoorSerial> \n"
+						+"<supplyAir>"+supplyAir +"</supplyAir><fluGasses>"+fluGasses+"</fluGasses>\n"
+						+ "</indoorTempRise>"+indoorTempRise+"</indoorTempRise> <outdoorTempRise>"+
+						outdoorTempRise+"</outdoorTempRise>\n"
+						+ "<indoorHigh>"+indoorHigh+"</indoorHigh> <indoorLow>"+indoorLow+"</indoorLow> " +
+						"<outdoorHigh>"+outdoorHigh+"</outdoorHigh> <outdoorLow>"+outdoorLow+"</outdoorLow> \n"
+						+"<gasPressure>"+gasPressure+"</gasPressure>\n"
+						+ "<filterLength>"+filterLength+"</filterLength> <filterWidth>"+filterWidth+"</filterWidth>"
+						+ "<filterHeight>"+filterHeight+"</filterHeight> </form>";
 				
 				try {
+					//File created under /data/data/[package name]/files/
 					FileOutputStream outputStream = openFileOutput(xmlFileName,
 					        Context.MODE_APPEND | Context.MODE_WORLD_READABLE); //only a security warning. It's fine
 					outputStream.write(xmlFile.getBytes());
@@ -86,6 +159,10 @@ public class NewForm extends Activity {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				
+				
+				//3 - Go back to main screen (avoids appending on two timestamps)
+				startActivity(new Intent(getApplicationContext(), MainActivity.class));
 				
 				
 			}
