15059cc62f0fc9848ed4671fa4122da3fec3a374
Squishmonsta <tuba.ritchie@yahoo.com>
2014-05-05 15:50:16 -0400

    here try this. XML parser

--- src/com/furnacechecks/ViewForms.java
+++ src/com/furnacechecks/ViewForms.java
@@ -1,12 +1,17 @@
 package com.furnacechecks;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -25,27 +30,51 @@ public class ViewForms extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_viewforms);
 		
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		//DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		
 		try {
-			
+			/*
 			DocumentBuilder builder = dbf.newDocumentBuilder();
-			File formListFile = new File("formList.xml");
-			//File formListFile = new File(getResources().getString(R.string.FormListFile));
+			//File formListFile = new File("formList.xml");
+			File formListFile = new File(getResources().getString(R.string.FormListFile));
 			
 			//The following line crashes it!! :(
 			Document formListDoc = builder.parse(formListFile);
-			/*
+			
 			formListDoc.normalize();
 			NodeList formNodes = formListDoc.getElementsByTagName("form");
+			
 			int counter = 0;
 			for(int i=0; i< formNodes.getLength(); i++){
 				counter++;
 			}
+			*/
+			File formListFile = new File(getResources().getString(R.string.FormListFile));
+			
+			//converts the xml files to a one line string
+			BufferedReader br = new BufferedReader(new FileReader(formListFile));
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while((line=br.readLine())!= null){
+			    sb.append(line.trim());
+			}
+			//XMLparser only need to check for <form> and </form>
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+	         factory.setNamespaceAware(true);
+	         XmlPullParser xpp = factory.newPullParser();
+
+	         xpp.setInput(new StringReader(line));
+	         int eventType = xpp.getEventType();
+	         int counter = 0;
+	         while (eventType != XmlPullParser.END_DOCUMENT) {
+	          if(eventType == XmlPullParser.START_DOCUMENT) {
+	              counter++;
+	          }
+	          eventType = xpp.next();
+	         }
 			
 			mOutput = "Test"+counter;
-			*/
-			mOutput = "TEST";
+			//mOutput = "TEST";
 			
 			mViewOutput = (TextView) findViewById(R.id.viewOutputText);
 			mTestButton = (Button) findViewById(R.id.testButton);
@@ -61,7 +90,6 @@ public class ViewForms extends Activity {
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
 		
 		//TODO: Open resource folder where forms are kept and display list of available forms
 		
